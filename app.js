dc923b9e98b6040de320c8293c4044ba1fab460b
Assaf Assa <assafas141@gmail.com>
2024-09-18 19:35:55 +0300

    cors again

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const app =express();
 const cors = require("cors");
 const corsOptions = {
   origin: "*",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",};
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true};
 app.use(cors(corsOptions));
 app.use(express.json());
 const recipesRoutes=require('./routes/recipesRoutes');
