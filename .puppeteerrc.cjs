a71074248d3dc41518e337bb4c1b060d951e188f
Assaf Assa <assafas141@gmail.com>
2024-10-04 16:24:53 +0300

    please

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -5,12 +5,11 @@ module.exports = {
     cacheDirectory: '/opt/render/.cache/puppeteer',
     chrome: {
       skipDownload: false,
-      // Explicitly specify the Chrome version Puppeteer should download
-      executablePath: 'chrome',
-      browserVersion: '129.0.6668.70',
+      // Explicitly specify the Chrome executable path
+      executablePath: '/opt/render/.cache/puppeteer/chrome/linux-129.0.6668.89/chrome-linux64/chrome', // Update the path as needed
+      browserVersion: '129.0.6668.70', // Make sure this matches the installed version
     },
     firefox: {
       skipDownload: false,
     },
-  };
-  
+};
