af5dc1b8284e349b03e090f1be788a6d746f8835
Assaf Assa <assafas141@gmail.com>
2024-10-04 14:56:49 +0300

    consolel og

--- puppeteer/searchRecipes.js
+++ puppeteer/searchRecipes.js
@@ -2,34 +2,35 @@ const puppeteer=require('puppeteer')
 //search
  async function searchRecipes(searchtype){
     console.log('Puppeteer Chrome Executable Path:', puppeteer.executablePath());
-    const browser = await puppeteer.launch({
-        
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        headless: true,
-      });
-    const page=await browser.newPage()
-    await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36")
-    await page.setViewport({ width: 1920, height: 5000 });
-    await page.goto(`https://www.allrecipes.com/search?q=${searchtype}`, {
-        waitUntil: 'networkidle2', // Wait for the network to be idle
-    });
-    const searchresults= await page.$$eval('body > main > div > div > div > a',(results)=>{
-        return results.map(x=>{
-            if (x.querySelector(".mntl-recipe-card-meta")){
-                return(
-                    {href:x.href,
-                    photosrc: x.querySelector("img").src,
-                    title: x.querySelector('div:nth-child(2) span span').textContent
-                    })  
-            }else {
-                return undefined
-            }
-            
-        }).filter(result => result !== undefined);
-    })
-    await browser.close()
     
-    return(searchresults)
+    // const browser = await puppeteer.launch({
+        
+    //     args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    //     headless: true,
+    //   });
+    // const page=await browser.newPage()
+    // await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36")
+    // await page.setViewport({ width: 1920, height: 5000 });
+    // await page.goto(`https://www.allrecipes.com/search?q=${searchtype}`, {
+    //     waitUntil: 'networkidle2', // Wait for the network to be idle
+    // });
+    // const searchresults= await page.$$eval('body > main > div > div > div > a',(results)=>{
+    //     return results.map(x=>{
+    //         if (x.querySelector(".mntl-recipe-card-meta")){
+    //             return(
+    //                 {href:x.href,
+    //                 photosrc: x.querySelector("img").src,
+    //                 title: x.querySelector('div:nth-child(2) span span').textContent
+    //                 })  
+    //         }else {
+    //             return undefined
+    //         }
+            
+    //     }).filter(result => result !== undefined);
+    // })
+    // await browser.close()
+    
+    // return(searchresults)
     
 }
 
